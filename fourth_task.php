d75f3f80c6a409f40c5bbe590cecc1e2075c3198
andrwdmnqz <105074672+andrwdmnqz@users.noreply.github.com>
2024-07-12 16:18:16 +0300

    changed algorithm and code-style

--- fourth_task.php
+++ fourth_task.php
@@ -7,15 +7,20 @@ function readHttpLikeInput() {
     $f = fopen( 'php://stdin', 'r' );
     $store = "";
     $toread = 0;
+
     while( $line = fgets( $f ) ) {
         $store .= preg_replace("/\r/", "", $line);
+
         if (preg_match('/Content-Length: (\d+)/',$line,$m)) 
             $toread=$m[1]*1; 
+
         if ($line == "\r\n") 
               break;
     }
+
     if ($toread > 0) 
         $store .= fread($f, $toread);
+
     return $store;
 }
 
@@ -31,12 +36,13 @@ $contents = readHttpLikeInput();
  */
 function outputHttpResponse($statuscode, $statusmessage, $headers, $body) {
     
-    echo "HTTP/1.1 $statuscode $statusmessage\n";
+    echo "HTTP/1.1 $statuscode $statusmessage" . PHP_EOL;
 
     foreach($headers as $key => $value) {
-        echo "$key: $value\n";
+        echo "$key: $value" . PHP_EOL;
     }
-    echo "\n$body";
+
+    echo PHP_EOL . $body;
 }
 
 /**
@@ -45,94 +51,105 @@ function outputHttpResponse($statuscode, $statusmessage, $headers, $body) {
  * 
  * @param string $method method of request
  * @param string $uri uri of request
- * @param string $headers headers of request
- * @param array $body body of request
+ * @param array $headers headers of request
+ * @param string $body body of request
  */
 function processHttpRequest($method, $uri, $headers, $body) {
-    $statuscode = "";
-    $statusMessage = "";
-    $responseBody = "";
-    $body = "";
+
     // Define needed headers
-    $generatedHeaders = array(
+    $generatedHeaders = [
         "Server" => "Apache/2.2.14 (Win32)",
         "Content-Length" => "",
         "Connection" => "Closed",
         "Content-Type" => "text/html; charset=utf-8"
-    );
-    
-    // If uri and header is correct
-    if ($uri === "/api/checkLoginAndPassword" and $headers["Content-Type"] === "application/x-www-form-urlencoded") {
-    
-        if (file_exists("passwords.txt")) {
-            $fileLines = explode("\n", file_get_contents("passwords.txt"));
-            
-            // Parse login and password from request
-            $login = substr($body, strpos($body, "=") + 1, strpos($body, "&") - strpos($body, "=") - 1);
-            $password = substr($body, strpos($body, "&") + strlen("&password="));
-        
-            foreach ($fileLines as $line) {
-                // Try to find match in the file. If found - 
-                // assign needed values to respone parts
-                $lineData = explode(":", trim($line));
-                if ($login === $lineData[0] and $password === $lineData[1]) {
-                    $responseBody = '<h1 style="color:green">FOUND</h1>';
-                    $statuscode = "200";
-                    $statusMessage = "OK";
-                }
-            }
-            
-            // If there is no match - unauthorized error
-            if ($responseBody === "") {
-                $responseBody = '<h1 style="color:green">NOT FOUND</h1>';
-                $statuscode = "401";
-                $statusMessage = "Unauthorized";
-            }
-        
-        } else {
-            // File not exists - 500 server error
-            $statuscode = "500";
-            $statusMessage = "Internal Server Error";
-        }
-    } else {
-        // If uri or header incorrect - return client error
+    ];
+
+    // If uri or header incorrect - return client error
+    if ($uri !== "/api/checkLoginAndPassword" || $headers["Content-Type"] !== "application/x-www-form-urlencoded") {
         $statuscode = "400";
         $statusMessage = "Bad Request";
         $responseBody = "not found";
+
+        $generatedHeaders["Content-Length"] = strlen((string)$responseBody);
+        outputHttpResponse($statuscode, $statusMessage, $generatedHeaders, $responseBody);
+
+        return;
     }
+
+    // If file not exists - 500 server error
+    if (!file_exists("passwords.txt")) {
+        $statuscode = "500";
+        $statusMessage = "Internal Server Error";
+        $responseBody = "not found";
+
+        $generatedHeaders["Content-Length"] = strlen((string)$responseBody);
+        outputHttpResponse($statuscode, $statusMessage, $generatedHeaders, $responseBody);
+
+        return;
+    }
+
+    $fileLines = explode(PHP_EOL, file_get_contents("passwords.txt"));
+            
+    // Parse login and password from request
+    $login = substr($body, strpos($body, "=") + 1, strpos($body, "&") - strpos($body, "=") - 1);
+    $password = substr($body, strpos($body, "&") + strlen("&password="));
         
+    foreach ($fileLines as $line) {
+        // Try to find match in the file. If found - 
+        // assign needed values to respone parts
+        $lineData = explode(":", trim($line));
+
+        if ($login === $lineData[0] and $password === $lineData[1]) {
+            $responseBody = '<h1 style="color:green">FOUND</h1>';
+            $statuscode = "200";
+            $statusMessage = "OK";
+
+            $generatedHeaders["Content-Length"] = strlen((string)$responseBody);
+            outputHttpResponse($statuscode, $statusMessage, $generatedHeaders, $responseBody);
+
+            return;
+        }
+    }
+
+    $statuscode = "401";
+    $statusMessage = "Unauthorized";
+    $responseBody = '<h1 style="color:red">NOT FOUND</h1>';
 
     $generatedHeaders["Content-Length"] = strlen((string)$responseBody);
     outputHttpResponse($statuscode, $statusMessage, $generatedHeaders, $responseBody);
 }
 
+/**
+ * Function parces given string to http request
+ * 
+ * @param string given tcp string
+ */
 function parseTcpStringAsHttpRequest($string) {
+    $splittedFirstLine = explode(" ", $string);
+    $method = $splittedFirstLine[0];
+    $uri = $splittedFirstLine[1];
 
-    $method = substr($string, 0, strpos($string, ' '));
-    $string = substr($string, strlen($method) + 1);
+    $string = substr($string, strpos($string, "\n") + strlen("\n"));
 
-    $uri = substr($string, 0, strpos($string, " "));
-    $string = substr($string, strpos($string, "\n") + 1);
-
-    $headers = array();
+    $headers = [];
+    // Split string by lines
     $lines = explode("\n", $string);
 
     foreach ($lines as $line) {
-        if ($line != "") {
+
+        if (strpos($line, ":")) {
+            // If line isn't empty - extract header from it
             $header_name = substr($line, 0, strpos($line, ":"));
-            $header_value = substr($line, strlen($header_name) + 2);
-            $headers[$header_name] = $header_value;
-        } else {
-            break;
+            $headers[$header_name] = substr($line, strlen($header_name) + 2);
         }
     }
     
-    return array(
+    return [
         "method" => $method,
         "uri" => $uri,
         "headers" => $headers,
         "body" => end($lines),
-    );
+    ];
 }
 
 $http = parseTcpStringAsHttpRequest($contents);
