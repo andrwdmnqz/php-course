8988a3982b779b724e96dad57043b7598098d780
andrwdmnqz <105074672+andrwdmnqz@users.noreply.github.com>
2024-07-12 12:21:01 +0300

    code style and early-return with validation first

--- third_task.php
+++ third_task.php
@@ -7,15 +7,20 @@ function readHttpLikeInput() {
     $f = fopen( 'php://stdin', 'r' );
     $store = "";
     $toread = 0;
+
     while( $line = fgets( $f ) ) {
         $store .= preg_replace("/\r/", "", $line);
+
         if (preg_match('/Content-Length: (\d+)/',$line,$m)) 
-            $toread=$m[1]*1; 
+            $toread=$m[1]*1;
+
         if ($line == "\r\n") 
               break;
     }
+
     if ($toread > 0) 
         $store .= fread($f, $toread);
+
     return $store;
 }
 
@@ -31,12 +36,13 @@ $contents = readHttpLikeInput();
  */
 function outputHttpResponse($statuscode, $statusmessage, $headers, $body) {
     
-    echo "HTTP/1.1 $statuscode $statusmessage\n";
+    echo "HTTP/1.1 $statuscode $statusmessage" . PHP_EOL;
 
     foreach($headers as $key => $value) {
-        echo "$key: $value\n";
+        echo "$key: $value" . PHP_EOL;
     }
-    echo "\n$body";
+
+    echo PHP_EOL . $body;
 }
 
 /**
@@ -52,67 +58,75 @@ function processHttpRequest($method, $uri, $headers, $body) {
     $statuscode = "";
     $statusMessage = "";
     $responseBody = "";
-    $body = "";
+
     // Define needed headers
-    $generatedHeaders = array(
+    $generatedHeaders = [
         "Server" => "Apache/2.2.14 (Win32)",
-        "Content-Length" => "",
         "Connection" => "Closed",
-        "Content-Type" => "text/html; charset=utf-8"
-    );
-    
-    // If uri and header is correct
-    if ($method === "GET" and str_starts_with($uri, "/sum?nums=")) {
-        $numbers = explode(",", substr($uri, strlen("/sum?nums=")));
-        $statuscode = "200";
-        $statusMessage = "OK";
-        $responseBody = array_sum($numbers);
-    }
+        "Content-Type" => "text/html; charset=utf-8",
+        "Content-Length" => "",
+    ];
 
     if (!str_starts_with($uri, "/sum")) {
         // If uri doesn't starts with sum - return client error
         $statuscode = "404";
         $statusMessage = "Not Found";
         $responseBody = "not found";
+
+        $generatedHeaders["Content-Length"] = strlen((string)$responseBody);
+        outputHttpResponse($statuscode, $statusMessage, $generatedHeaders, $responseBody);
+
+        return;
     }
-    if (!strpos($uri, "?nums=") or $method != "GET") {
+
+    if (!strpos($uri, "?nums=") || $method !== "GET") {
         // If uri or header incorrect - return client error
         $statuscode = "400";
         $statusMessage = "Bad Request";
         $responseBody = "bad request";
+
+        $generatedHeaders["Content-Length"] = strlen((string)$responseBody);
+        outputHttpResponse($statuscode, $statusMessage, $generatedHeaders, $responseBody);
+
+        return;
     }
+    
+    // If uri and header are correct
+    $numbers = explode(",", substr($uri, strlen("/sum?nums=")));
+    $statuscode = "200";
+    $statusMessage = "OK";
+    $responseBody = array_sum($numbers);
 
     $generatedHeaders["Content-Length"] = strlen((string)$responseBody);
     outputHttpResponse($statuscode, $statusMessage, $generatedHeaders, $responseBody);
 }
 
 function parseTcpStringAsHttpRequest($string) {
+    $splittedFirstLine = explode(" ", $string);
+    $method = $splittedFirstLine[0];
+    $uri = $splittedFirstLine[1];
 
-    $method = substr($string, 0, strpos($string, ' '));
-    $string = substr($string, strlen($method) + 1);
+    $string = substr($string, strpos($string, "\n") + strlen("\n"));
 
-    $uri = substr($string, 0, strpos($string, " "));
-    $string = substr($string, strpos($string, "\n") + 1);
-
-    $headers = array();
+    $headers = [];
+    // Split string by lines
     $lines = explode("\n", $string);
 
     foreach ($lines as $line) {
-        if ($line != "") {
+
+        if (strpos($line, ":")) {
+            // If line isn't empty - extract header from it
             $header_name = substr($line, 0, strpos($line, ":"));
-            $header_value = substr($line, strlen($header_name) + 2);
-            $headers[$header_name] = $header_value;
-        } else {
-            break;
+            $headers[] = array($header_name, substr($line, strlen($header_name) + 2));
         }
     }
     
-    return array(
+    return [
         "method" => $method,
         "uri" => $uri,
         "headers" => $headers,
         "body" => end($lines),
-    );
+    ];
 }
 
 $http = parseTcpStringAsHttpRequest($contents);
