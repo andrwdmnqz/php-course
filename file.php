f2d4904cc8cd188fb2f7810abacad577d102e677
andrwdmnqz <105074672+andrwdmnqz@users.noreply.github.com>
2024-07-16 20:39:26 +0300

    renamed file and added creation of .txt file if it isn't exists

--- file.php
+++ file.php
@@ -5,9 +5,19 @@
  * 
  */
 function incrementClicksCounter() {
-    $clicksCounter = trim(file_get_contents("clicks_counter.txt"));
 
+    $counterFile = "clicks_counter.txt";
+
+    // If file not exists - create it and put "0"
+    if (!file_exists($counterFile)) {
+        file_put_contents($counterFile, "0");
+    }
+
+    // Get saved value and increment it
+    $clicksCounter = trim(file_get_contents($counterFile));
     $clicksCounter++;
+
+    // Save new value
     file_put_contents("clicks_counter.txt", $clicksCounter);
 
     showClicksCounter($clicksCounter);
